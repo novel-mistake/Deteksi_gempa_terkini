74efba9e4a995ec6ab0e3a1410fb084a42def425
novelmisatake <fteguh088@gmail.com>
2023-09-19 13:05:28 +0800

    22

--- main.py
+++ main.py
@@ -3,44 +3,9 @@ Aplikasi deteksi gempa terkini
 MODULARISAI DENGAN FUNCTION
 """
 
-
-def ekstraksi_data():
-    """
-Tanggal: 17 September 2023
-Waktu: 07:44:20 WIB
-Magnitudo: 4.2
-Kedalaman: 5km
-Lokasi: LS=3.34  BT= 97.90
-Pusat gempa: berada di darat 18 km Tenggara Kutacane
-Dirasakan:  (Skala MMI): II-III Aceh Tenggara
-    :return:
-    """
-    hasil = dict()
-    hasil['tanggal'] = '17 September 2023'
-    hasil['Waktu'] = '07:44:20 WIB'
-    hasil['Magnitudo'] = '4.2'
-    hasil['Kedalaman'] = '5 km'
-    hasil['Lokasi'] = {'ls': 3.34, 'bt': 97.90}
-    hasil['Pusat gempa'] = 'berada di darat 18 km Tenggara Kutacane'
-    hasil['Dirasakan'] = '(Skala MMI): II-III Aceh Tenggara'
-    return hasil
-
-
-def tampilkan_data(result):
-    print('gempa terakhir berdasarkan BMKG')
-    print(f"tanggal {result['tanggal']}")
-    print(f"Waktu {result['Waktu']}")
-    print(f"Magnitudo {result['Magnitudo']}")
-    print(f"Kedalaman {result['Kedalaman']}")
-    print(f"Lokasi: LS={result['Lokasi']['ls']},BR={result['Lokasi']['bt']}")
-    print(f"Pusat gempa {result['Pusat gempa']}")
-    print(f"Dirasakan {result['Dirasakan']}")
-
-
+import gempa_terkini
 
 if __name__ == '__main__':
     print('Aplikasi utama')
-    result = ekstraksi_data()
-    tampilkan_data(result)
-
-print('test')
+    result = gempa_terkini.ekstraksi_data()
+    gempa_terkini.tampilkan_data(result)
